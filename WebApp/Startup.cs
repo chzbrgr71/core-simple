c4125881e37d40d11af735d261b66e00bfe93b5b
Brian Redmond <brianisrunning@gmail.com>
2017-04-25 17:49:23 -0700

    updated to 3.1.3

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -31,7 +31,7 @@ namespace WebApp
             app.Run(async (context) =>
             {
                 // Gather values for frontend service 
-                string appVersion = "3.1.2";
+                string appVersion = "3.1.3";
                 string frontendName = Environment.MachineName;
                 var ips = Dns.GetHostAddressesAsync(frontendName).Result;
                 var addresses = "";
